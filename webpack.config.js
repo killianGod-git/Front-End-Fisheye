9d54ecf9f591b7c6929ee40c404a482a40fab4b1
Killian Godmez <kgodmez@gmail.com>
2022-09-08 19:53:16 +0200

    ajout webpack & WIP fetch

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/scripts/app.js'),
+    photographer: path.resolve(__dirname, './src/scripts/pages/photographer.js')
+  },
+  output: {
+    path: path.resolve(__dirname, './src/dist'),
+    filename: '[name].bundle.js',
+  },
+}
